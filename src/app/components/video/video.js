d27f5f159810933ba41c8a00375634dd77771daa
Denis Merigoux <denis.merigoux@gmail.com>
2015-07-01 22:21:51 +0200

    Failed to make correct directive

--- src/app/components/video/video.js
+++ src/app/components/video/video.js
@@ -1,52 +1,57 @@
 'use strict';
 
 angular.module('jtx.video', [
-    //
+  //
 ])
 
 .config(['$stateProvider',
-    function($stateProvider) {
-        $stateProvider
-            .state('index.video', {
-                url: 'video',
-                templateUrl: 'app/components/video/video.html',
-                controller: 'video.ctrl'
-            })
-        ;
-    }
+function($stateProvider) {
+  $stateProvider
+  .state('index.video', {
+    url: 'video',
+    templateUrl: 'app/components/video/video.html',
+    controller: 'video.ctrl'
+  })
+  ;
+}
 ])
 
 .controller('video.ctrl',
-    ['$scope',
-    function($scope) {;
-      //
-    }
+['$scope',
+function($scope) {;
+  $scope.demons = {
+    files : [
+      { resolution : "1080",
+      src : "assets/videos/demonsdesylvie.mp4",
+      extension : "mp4",
+      subtitles : "assets/videos/demonsdesylvie.vtt",
+      }
+    ],
+    id : "1"
+  }
+}
 ])
 
-.directive('videoPlayer',function() {
+.directive('jtxVideoPlayer',['$timeout', function(executeAfterDOMRendering) {
 
-  var video;
-
-  function link(scope,element,attrs) {
-    attrs.$observe('src',function (value){
-      video = value;
-      videojs("video-"+"4", {
-        controls : true,
-        preload : 'metadata',
-        width : '680',
-        height : '440',
-        plugins : {
-           "resolutionSelector" : { "default_res" : "480" }
-           }
-        }, function(){
-  // Player (this) is initialized and ready.
-      });
-    });
-  }
+  function link(scope) {
+    executeAfterDOMRendering(videojs("video-"+scope.videoObject.id, {
+      controls : true,
+      preload : 'metadata',
+      width : '680',
+      height : '440',
+      plugins : {
+        "resolutionSelector" : { "default_res" : "480" }
+      }
+    }, function(){
+      // Player (this) is initialized and ready.
+    }));
+  };
 
   return {
-     restrict : 'E',
-     templateUrl : 'app/components/video/video-player.html',
-  }
-})
-;
+    restrict : 'E',
+    templateUrl : 'app/components/video/video-player.html',
+    link : link,
+    scope: { videoObject: '=' },
+  };
+}]);
