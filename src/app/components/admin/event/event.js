7f2a3943efbebf871d6578836cfa644f9b2fe69a
tizot <masset.camille@gmail.com>
2015-07-06 22:06:32 +0200

    Refactor event addition

--- src/app/components/admin/event/event.js
+++ src/app/components/admin/event/event.js
@@ -1,42 +1,39 @@
 angular.module('jtx.admin.event', [
-  //
+    //
 ])
 
-.config(['$stateProvider',
-function($stateProvider) {
-  $stateProvider
-  .state('index.admin.event', {
-    url: '/event',
-    abstract : true,
-    template : '<ui-view />',
-  })
-  .state('index.admin.event.add', {
-    url: '/add',
-    templateUrl : 'app/components/admin/event/add.html',
-    controller: 'admin.event.add.ctrl',
-  })
-  ;
-}
+.config(['$stateProvider', 
+    function($stateProvider) {
+        $stateProvider
+            .state('index.admin.event', {
+                url: '/event',
+                abstract : true,
+                template : '<ui-view />',
+            })
+            .state('index.admin.event.add', {
+                url: '/add',
+                templateUrl : 'app/components/admin/event/add.html',
+                controller: 'admin.event.add.ctrl',
+            })
+        ;
+    }
 ])
 
 .controller('admin.event.add.ctrl',
-['$scope', '$resource', 'Event' , function($scope,$resource,Event) {
-  $scope.event = new Event();
-  $scope.event.title = "TSGED 2015";
-  $scope.event.description = "Tournoi Sportif des Grandes Écoles de la Défense";
-  $scope.event_beginDate = new Date('2015','3','21','8','0','0');
-  $scope.event_endDate = new Date('2015','3','22','15','0','0');
+    ['$scope', '$resource', 'Event', 
+    function($scope,$resource,Event) {
+        $scope.event = new Event();
+        $scope.event.beginDate_f = new Date();
+        $scope.event.endDate_f = new Date();
 
-  $scope.addEvent = function (event) {
-    event.beginDate = $scope.event_beginDate.toISOString();
-    event.endDate = $scope.event_endDate.toISOString();
-    Event.save({"event": $scope.event}).then(function(result) {
-      $scope.success =  "L'événement a été ajouté avec succès !";
-    },
-    function(errors) {
-      $scope.errors = errors;
-  });
-
-};
-}
-]);
+        $scope.addEvent = function (event) {
+            event.beginDate = $scope.event.beginDate_f.toJSON();
+            event.endDate = $scope.event.endDate_f.toJSON();
+            Event.save($scope.event).then(function(result) {
+                $scope.success =  "L'événement a été ajouté avec succès !";
+            }, function(errors) {
+                $scope.errors = errors;
+            });
+        };
+    }]
+);
