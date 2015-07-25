a765312e9fa4049b3b5975546d9891a8aba91a9d
Denis Merigoux <denis.merigoux@gmail.com>
2015-07-25 22:30:50 +0200

    Add and modify events : OK Server-side is bugged

--- src/app/components/admin/event/event.js
+++ src/app/components/admin/event/event.js
@@ -15,6 +15,11 @@ function($stateProvider) {
     templateUrl : 'app/components/admin/event/add.html',
     controller: 'admin.event.add.ctrl',
   })
+  .state('index.admin.event.edit', {
+    url: '/edit/:eventId',
+    templateUrl : 'app/components/admin/event/edit.html',
+    controller: 'admin.event.edit.ctrl',
+  })
   ;
 }
 ])
@@ -23,45 +28,103 @@ function($stateProvider) {
 ['$scope', '$resource', 'Event',
 function($scope,$resource,Event) {
   $scope.event = new Event();
-  $scope.now = new Date();
+  $scope.now = new moment();
   $scope.showSuccess = false;
   $scope.showErrors = false;
-
-  $scope.openBegin = function($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-    $scope.datepickerBegin = true;
-  };
-
-  $scope.openEnd = function($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-    $scope.datepickerEnd = true;
-  };
 
   $scope.addEvent = function (event) {
     var sentEvent = {title : event.title, description : event.description};
 
-    var beginHours = $scope.event.beginTime_f.split(":")[0];
-    var beginMinutes = $scope.event.beginTime_f.split(":")[1];
-    event.beginDate_f.setHours(beginHours);
-    event.beginDate_f.setMinutes(beginMinutes);
-    sentEvent.beginDate = $scope.event.beginDate_f.toJSON();
+    //Merge the date and time fields into one datetime JSON string
+    var beginHours = event.beginTime_f.split(":")[0];
+    var beginMinutes = event.beginTime_f.split(":")[1];
+    sentEvent.begin_date = moment(event.beginDate_f);
+    sentEvent.begin_date.hours(beginHours);
+    sentEvent.begin_date.minutes(beginMinutes);
+    sentEvent.begin_date = sentEvent.begin_date.toJSON();
 
-    var endHours = $scope.event.endTime_f.split(":")[0];
-    var endMinutes = $scope.event.endTime_f.split(":")[1];
-    event.endDate_f.setHours(endHours);
-    event.endDate_f.setMinutes(endMinutes);
-    sentEvent.endDate = $scope.event.endDate_f.toJSON();
+    var endHours = event.endTime_f.split(":")[0];
+    var endMinutes = event.endTime_f.split(":")[1];
+    sentEvent.end_date = moment(event.endDate_f);
+    sentEvent.end_date.hours(endHours);
+    sentEvent.end_date.minutes(endMinutes);
+    sentEvent.end_date = sentEvent.end_date.toJSON();
 
-    console.log(sentEvent);
     Event.save(sentEvent).$promise.then(function(result) {
       $scope.showSuccess = true;
-      $scope.success =  "L'événement a été ajouté avec succès !";
+      $scope.success =  "L'événement "+ Event.title+" a été ajouté avec succès !";
     }, function(errors) {
       $scope.showErrors= true;
       $scope.errors = errors;
     });
+  };
+}]
+)
+
+.controller('admin.event.edit.ctrl',
+['$scope', '$resource', 'Event', '$stateParams', '$window',
+function($scope,$resource,Event, $stateParams, $window) {
+  Event.get({id:$stateParams.eventId}, function(event) {
+    $scope.event = event;
+
+    //Hydrates the date and time fields of the form with the data
+    var beginDate_f = moment(event.begin_date);
+    $scope.event.beginDate_f = beginDate_f.format('YYYY-MM-DD');
+    $scope.event.beginTime_f = beginDate_f.add(2,'h').format('HH:mm');//TODO delete add(2,'h')
+
+    var endDate_f= moment(event.end_date);
+    $scope.event.endDate_f = endDate_f.format('YYYY-MM-DD');
+    $scope.event.endTime_f = endDate_f.add(2,'h').format('HH:mm');
+
+    $scope.event.updated_at = moment(event.updated_at);
+  });
+
+  $scope.now = new Date();
+  $scope.showSuccess = false;
+  $scope.showErrors = false;
+
+  $scope.editEvent = ['event', function(event) {
+    var sentEvent = {title : event.title, description : event.description, id: event.id};
+
+    //Merge the date and time fields into one datetime JSON string
+    var beginHours = event.beginTime_f.split(":")[0];
+    var beginMinutes = event.beginTime_f.split(":")[1];
+    sentEvent.begin_date = moment(event.beginDate_f);
+    sentEvent.begin_date.hours(beginHours);
+    sentEvent.begin_date.minutes(beginMinutes);
+    sentEvent.begin_date = sentEvent.begin_date.toJSON();
+
+    var endHours = event.endTime_f.split(":")[0];
+    var endMinutes = event.endTime_f.split(":")[1];
+    sentEvent.end_date = moment(event.endDate_f);
+    sentEvent.end_date.hours(endHours);
+    sentEvent.end_date.minutes(endMinutes);
+    sentEvent.end_date = sentEvent.end_date.toJSON();
+
+    Event.update(sentEvent).$promise.then(function(result) {
+      $scope.showSuccess = true;
+      $scope.success =  "L'événement a été mis à jour avec succès !";
+    }, function(errors) {
+      $scope.showErrors= true;
+      $scope.errors = errors;
+    });
+  }];
+
+  $scope.deleteEvent = function(eventToDelete) {
+    if ($window.confirm("Voulez-vous vraiment supprimer cet événement ?"))
+      {
+        Event.delete(eventToDelete.id).$promise.then(
+          function() {
+            $scope.event = null;
+            $scope.showSuccess = true;
+            $scope.success =  "L'événement a supprimé avec succès !";
+          },
+          function(errors) {
+            $scope.showErrors= true;
+            $scope.errors = errors;
+          }
+        )
+      }
   };
 }]
 );
--- src/app/components/common/main.js
+++ src/app/components/common/main.js
@@ -1,76 +1,137 @@
 'use strict';
 
 angular.module('jtx.main', [
-    //
+  //
 ])
 
 .config(['$stateProvider',
-    function($stateProvider) {
-        $stateProvider
-            .state('index', {
-                url: '/',
-                views: {
-                    '@': {
-                        templateUrl: 'app/components/common/layout.html',
-                        controller: 'main.ctrl.base'
-                    },
-                    'navbar@index': {
-                        templateUrl: 'app/components/common/navbar.html',
-                        controller: 'main.ctrl.navbar'
-                    },
-                    '@index': {
-                        templateUrl: 'app/components/common/home.html',
-                        controller: 'main.ctrl.home'
-                    }
-                }
-            })
-            .state('index.binet', {
-                url: 'binet',
-                templateUrl: 'app/components/static/binet.html'
-            })
-            .state('index.legal', {
-                url: 'legal',
-                templateUrl: 'app/components/static/legal.html'
-            })
-        ;
+function($stateProvider) {
+  $stateProvider
+  .state('index', {
+    url: '/',
+    views: {
+      '@': {
+        templateUrl: 'app/components/common/layout.html',
+        controller: 'main.ctrl.base'
+      },
+      'navbar@index': {
+        templateUrl: 'app/components/common/navbar.html',
+        controller: 'main.ctrl.navbar'
+      },
+      '@index': {
+        templateUrl: 'app/components/common/home.html',
+        controller: 'main.ctrl.home'
+      }
     }
+  })
+  .state('index.binet', {
+    url: 'binet',
+    templateUrl: 'app/components/static/binet.html'
+  })
+  .state('index.legal', {
+    url: 'legal',
+    templateUrl: 'app/components/static/legal.html'
+  })
+  ;
+}
 ])
 
 .controller('main.ctrl.base',
-    ['$scope',
-    function($scope) {
-        $scope.hello = 'Hello';
+['$scope',
+function($scope) {
+  $scope.hello = 'Hello';
+  moment.locale('fr', {
+    months : "janvier_février_mars_avril_mai_juin_juillet_août_septembre_octobre_novembre_décembre".split("_"),
+    monthsShort : "janv._févr._mars_avr._mai_juin_juil._août_sept._oct._nov._déc.".split("_"),
+    weekdays : "dimanche_lundi_mardi_mercredi_jeudi_vendredi_samedi".split("_"),
+    weekdaysShort : "dim._lun._mar._mer._jeu._ven._sam.".split("_"),
+    weekdaysMin : "Di_Lu_Ma_Me_Je_Ve_Sa".split("_"),
+    longDateFormat : {
+      LT : "HH:mm",
+      LTS : "HH:mm:ss",
+      L : "DD/MM/YYYY",
+      LL : "D MMMM YYYY",
+      LLL : "D MMMM YYYY LT",
+      LLLL : "dddd D MMMM YYYY LT"
+    },
+    calendar : {
+      sameDay: "[Aujourd'hui à] LT",
+      nextDay: '[Demain à] LT',
+      nextWeek: 'dddd [à] LT',
+      lastDay: '[Hier à] LT',
+      lastWeek: 'dddd [dernier à] LT',
+      sameElse: 'L'
+    },
+    relativeTime : {
+      future : "dans %s",
+      past : "il y a %s",
+      s : "quelques secondes",
+      m : "une minute",
+      mm : "%d minutes",
+      h : "une heure",
+      hh : "%d heures",
+      d : "un jour",
+      dd : "%d jours",
+      M : "un mois",
+      MM : "%d mois",
+      y : "une année",
+      yy : "%d années"
+    },
+    ordinalParse : /\d{1,2}(er|ème)/,
+    ordinal : function (number) {
+      return number + (number === 1 ? 'er' : 'ème');
+    },
+    meridiemParse: /PD|MD/,
+    isPM: function (input) {
+      return input.charAt(0) === 'M';
+    },
+    // in case the meridiem units are not separated around 12, then implement
+    // this function (look at locale/id.js for an example)
+    // meridiemHour : function (hour, meridiem) {
+    //     return /* 0-23 hour, given meridiem token and hour 1-12 */
+    // },
+    meridiem : function (hours, minutes, isLower) {
+      return hours < 12 ? 'PD' : 'MD';
+    },
+    week : {
+      dow : 1, // Monday is the first day of the week.
+      doy : 4  // The week that contains Jan 4th is the first week of the year.
     }
+  });
+  moment.locale('fr');
+}
 ])
 
 .controller('main.ctrl.navbar',
-    ['$scope',
-    function($scope) {
-        $scope.search = '';
+['$scope',
+function($scope) {
+  $scope.search = '';
 }])
 
 .controller('main.ctrl.home',
-    ['$scope',
-    function($scope) {
-        //
-    }
+['$scope',
+function($scope) {
+  //
+}
 ])
 
 .directive('jtxSearchable', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'app/components/common/searchable-directive.html',
-        scope: {
-            kind: '=kind', // video | projection | event
-            object: '='
-        },
-        controller: ['$scope', function($scope) {
-          console.log($scope.kind);
-            if ($scope.kind == null)
-                $scope.kind = 'video';
-            else if ($scope.kind == 'event')
-                $scope.object.date = $scope.object.begin_date;
-        }]
-    }
+  return {
+    restrict: 'E',
+    templateUrl: 'app/components/common/searchable-directive.html',
+    scope: {
+      kind: '=kind', // video | projection | event
+      object: '='
+    },
+    controller: ['$scope', function($scope) {
+      $scope.object.begin_date = moment($scope.object.begin_date);
+      $scope.object.end_date = moment($scope.object.end_date);
+      console.log($scope.object.begin_date);
+      if ($scope.kind == null)
+      $scope.kind = 'video';
+      else if ($scope.kind == 'event')
+      $scope.object.date = $scope.object.begin_date;
+    }]
+  }
 })
 ;
