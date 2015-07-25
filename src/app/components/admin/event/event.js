84eb2b22a7c5b11ad89d864ca578b511fb2f71cd
Denis Merigoux <denis.merigoux@gmail.com>
2015-07-25 17:36:12 +0200

    Add event form fully fonctionnal Including data validation

--- src/app/components/admin/event/event.js
+++ src/app/components/admin/event/event.js
@@ -22,8 +22,10 @@ function($stateProvider) {
 .controller('admin.event.add.ctrl',
 ['$scope', '$resource', 'Event',
 function($scope,$resource,Event) {
-  $scope.activePanel = "addTag";
   $scope.event = new Event();
+  $scope.now = new Date();
+  $scope.showSuccess = false;
+  $scope.showErrors = false;
 
   $scope.openBegin = function($event) {
     $event.preventDefault();
@@ -38,13 +40,26 @@ function($scope,$resource,Event) {
   };
 
   $scope.addEvent = function (event) {
-    $scope.event.beginDate_f = new Date();
-    $scope.event.endDate_f = new Date();
-    event.beginDate = $scope.event.beginDate_f.toJSON();
-    event.endDate = $scope.event.endDate_f.toJSON();
-    Event.save($scope.event).$promise.then(function(result) {
+    var sentEvent = {title : event.title, description : event.description};
+
+    var beginHours = $scope.event.beginTime_f.split(":")[0];
+    var beginMinutes = $scope.event.beginTime_f.split(":")[1];
+    event.beginDate_f.setHours(beginHours);
+    event.beginDate_f.setMinutes(beginMinutes);
+    sentEvent.beginDate = $scope.event.beginDate_f.toJSON();
+
+    var endHours = $scope.event.endTime_f.split(":")[0];
+    var endMinutes = $scope.event.endTime_f.split(":")[1];
+    event.endDate_f.setHours(endHours);
+    event.endDate_f.setMinutes(endMinutes);
+    sentEvent.endDate = $scope.event.endDate_f.toJSON();
+
+    console.log(sentEvent);
+    Event.save(sentEvent).$promise.then(function(result) {
+      $scope.showSuccess = true;
       $scope.success =  "L'événement a été ajouté avec succès !";
     }, function(errors) {
+      $scope.showErrors= true;
       $scope.errors = errors;
     });
   };
