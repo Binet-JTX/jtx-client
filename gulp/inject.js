b8801e9ceb980aad79d973f64ab5eff1c9c95cda
tizot <masset.camille@gmail.com>
2015-09-08 18:03:40 +0200

    Try to fix gulp

--- gulp/inject.js
+++ gulp/inject.js
@@ -1,34 +1,35 @@
 'use strict';
 
+var path = require('path');
 var gulp = require('gulp');
+var conf = require('./conf');
 
 var $ = require('gulp-load-plugins')();
 
 var wiredep = require('wiredep').stream;
+var _ = require('lodash');
 
-module.exports = function(options) {
-  gulp.task('inject', ['scripts'], function () {
-    var injectStyles = gulp.src([
-      options.src + '/app/**/*.css'
-    ], { read: false });
+gulp.task('inject', ['scripts'], function () {
+  var injectStyles = gulp.src([
+    path.join(conf.paths.src, '/app/**/*.css')
+  ], { read: false });
 
-    var injectScripts = gulp.src([
-      options.src + '/app/**/*.js',
-      '!' + options.src + '/app/**/*.spec.js',
-      '!' + options.src + '/app/**/*.mock.js'
-    ])
-    .pipe($.angularFilesort()).on('error', options.errorHandler('AngularFilesort'));
+  var injectScripts = gulp.src([
+    path.join(conf.paths.src, '/app/**/*.module.js'),
+    path.join(conf.paths.src, '/app/**/*.js'),
+    path.join('!' + conf.paths.src, '/app/**/*.spec.js'),
+    path.join('!' + conf.paths.src, '/app/**/*.mock.js')
+  ])
+  .pipe($.angularFilesort()).on('error', conf.errorHandler('AngularFilesort'));
 
-    var injectOptions = {
-      ignorePath: [options.src, options.tmp + '/serve'],
-      addRootSlash: false
-    };
+  var injectOptions = {
+    ignorePath: [conf.paths.src, path.join(conf.paths.tmp, '/serve')],
+    addRootSlash: false
+  };
 
-    return gulp.src(options.src + '/*.html')
-      .pipe($.inject(injectStyles, injectOptions))
-      .pipe($.inject(injectScripts, injectOptions))
-      .pipe(wiredep(options.wiredep))
-      .pipe(gulp.dest(options.tmp + '/serve'));
-
-  });
-};
+  return gulp.src(path.join(conf.paths.src, '/*.html'))
+    .pipe($.inject(injectStyles, injectOptions))
+    .pipe($.inject(injectScripts, injectOptions))
+    .pipe(wiredep(_.extend({}, conf.wiredep)))
+    .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve')));
+});
